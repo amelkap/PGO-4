cdfb172b32318fc04d4226eaedf7683116040ff6
amelia <s35283@pjwstk.edu.pl>
2026-04-02 17:12:40 +0200

    Stworzenie Main odpowiadajacego do zadania

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BiuroUbezpieczen biuro = new BiuroUbezpieczen("Secure Future");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Polisa p1 = new Polisa("CAR-101", "Anna Nowak", 900.0, 3, 72000.0, true, true);
+        Polisa p2 = new Polisa("CAR-102", "Piotr Lis", 840.0, 4, 54000.0, false, false);
+        Polisa p3 = new Polisa("CAR-103", "Karolina Maj", 780.0, 2, 46000.0, true, false);
+
+        biuro.dodajPolise(p1);
+        biuro.dodajPolise(p2);
+        biuro.dodajPolise(p3);
+
+        biuro.wypiszRaport();
+        System.out.println("Laczna skladka: " + biuro.policzLacznaSkladke());
+        System.out.println("Laczna prognoza odnowien: " + biuro.policzLacznaPrognozeOdnowien());
+        System.out.println("Polisy wysokiego ryzyka: " + biuro.policzPolisyWysokiegoRyzyka());
+        System.out.println("Liczba utworzonych polis: " + Polisa.getLiczbeUtworzonychPolis());
+
+        System.out.println(p1.pobierzPodsumowanieRyzyka());
+        System.out.println("Prognoza odnowienia dla p1: " + p1.obliczSkladkeOdnowienia());
+        System.out.println(p1.equals(new Polisa("CAR-101", "Inny klient", 700.0, 1, 30000.0, false, false)));
+        System.out.println(p2);
+
+        Polisa znaleziona = biuro.znajdzPoNumerze("CAR-102");
+        System.out.println("Znaleziono: " + znaleziona);
     }
 }
